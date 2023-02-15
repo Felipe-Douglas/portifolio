c83de0d4cda6e843328c17981e6979c36ac95665
Filipe Douglas <felipe.douglas.inf1@gmail.com>
2023-02-15 15:54:44 -0300

    Portifolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,52 @@
-//Fixed Menu
-var nav = $('.nav-bar');
-$(document).on("scroll", () => {
-    console.log(window.scrollY);
-    if(window.scrollY >= 10) {
-        nav.addClass('nav-fixed');
-    } else {
-        nav.removeClass('nav-fixed');
-    }
-});
+$(document).ready(() => {
+    //Fixed Menu
+    var nav = $('.nav-bar');
+    $(document).on("scroll", () => {
+        if (window.scrollY >= 10) {
+            nav.addClass('nav-fixed');
+        } else {
+            nav.removeClass('nav-fixed');
+        }
+    });
 
-// Scroll
-$('nav a').click(function (item) {
-    item.preventDefault();
+    // Menu Mobile
+    $('.menu-mobile').on("click", () => {
+        $('.navigation').toggleClass('active-navigation');
+    });
 
-    var id = $(this).attr('href');
-    targetOffset = $(id).offset().top;
-    $('html, body').animate({
-        scrollTop: targetOffset
-    }, 400);
+    $('.navigation').on("click", () => {
+        $('.navigation').removeClass('active-navigation');
+    });
+
+    // Scroll
+    $('nav a').click(function (item) {
+        item.preventDefault();
+
+        var id = $(this).attr('href');
+        targetOffset = $(id).offset().top;
+        $('html, body').animate({
+            scrollTop: targetOffset
+        }, 400);
+    });
+
+    //Data
+    var data = new Date();
+    $('.year').html(data.getFullYear());
 });
 
 // Biblioteca de digitaçã
 document.addEventListener('DOMContentLoaded', () => {
     new TypeIt(".profile-skills", {
-        loop: true,
-    })
-    .type("Desenvolvedor Fullstack", {delay: 800}).delete(9)
-    .type("Web", {delay: 800}).delete(3)
-    .type("Mobile", {delay: 800}).delete(20)
-    .go();
+            loop: true,
+        })
+        .type("Desenvolvedor Fullstack", {
+            delay: 800
+        }).delete(9)
+        .type("Web", {
+            delay: 800
+        }).delete(3)
+        .type("Mobile", {
+            delay: 800
+        }).delete(20)
+        .go();
 });
